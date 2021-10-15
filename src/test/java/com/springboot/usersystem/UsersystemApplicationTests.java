964bbe65dfce03de67753ba700b64b734d1f8d7c
qiuqiuzhong <2896054615@qq.com>
2021-10-15 19:25:16 +0800

    first commit

---
+++ src/test/java/com/springboot/usersystem/UsersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.usersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
