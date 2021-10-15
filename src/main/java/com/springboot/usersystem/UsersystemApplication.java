db0c953f792beef397b77fe0c51938984229b556
qiuqiuzhong <2896054615@qq.com>
2021-10-15 20:06:57 +0800

    third commit2

--- src/main/java/com/springboot/usersystem/UsersystemApplication.java
+++ src/main/java/com/springboot/usersystem/UsersystemApplication.java
@@ -9,6 +9,7 @@ public class UsersystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(UsersystemApplication.class, args);
         System.out.println("123456");
+        System.out.println("hello word");
     }
 
 }
